d17cc993554286236782fcc4d5a3eca1edeb70a4
equinoxster <themock@gmail.com>
2025-05-09 21:28:17 +0300

    adds method to count how many char instances there are

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 def main():
-     words = get_num_words("./books/frankenstein.txt")
+     path = "./books/frankenstein.txt"
+     text = get_book_text(path)
+     words = get_num_words(text)
      
      print(f"{words} words found in the document")
+     char_dict = get_char_count(text)
+     print(char_dict)
 
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        # do something with file here
+        file_contents = f.read()
+        return file_contents
+    
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,13 @@
-def get_num_words(path_to_file) :
-    with open(path_to_file) as f:
-        # do something with file here
-        file_contents = f.read()
-        word_count = len(file_contents.split())
-        return word_count
+def get_num_words(text) :
+    word_count = len(text.split())
+    return word_count
+    
+def get_char_count(text):
+    lower_case_text = text.lower()
+    chars = {}
+    for c in lower_case_text:
+        if c in chars:
+            chars[c] += 1
+        else:
+            chars[c] = 1
+    return chars
