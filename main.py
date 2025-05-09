e98f54ce5be5333bcb6d558d9f6278f1ce0f2aa5
equinoxster <themock@gmail.com>
2025-05-10 00:07:19 +0300

    adds reading book path from arguments

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from stats import get_num_words, get_char_count, pretty_print
+import sys
 
 def main():
-    path = "books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     text = get_book_text(path)
     words = get_num_words(text)
      
